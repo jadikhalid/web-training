c80f21b3fc26b2617f56d9b981bd24021d253974
Jadi Khalid <jadikhalid@gmail.com>
2026-03-12 11:33:09 +0000

    ok

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,19 @@
-import { Button, Container, Flex, HStack, Text } from "@chakra-ui/react";
+import {
+  Button,
+  Container,
+  Flex,
+  HStack,
+  Text,
+  useColorMode,
+} from "@chakra-ui/react";
 import React from "react";
 import { Link } from "react-router-dom";
 import { PlusSquareIcon } from "@chakra-ui/icons";
+import { IoMoon } from "react-icons/io5";
+import { LuSun } from "react-icons/lu";
 
 const Navbar = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
   return (
     <Container maxW={"1140px"} px={4}>
       <Flex
@@ -17,11 +27,10 @@ const Navbar = () => {
           fontWeight={"bold"}
           textTransform={"uppercase"}
           textAlign={"center"}
-          bgGradient={"linear(to-r, cyan.400, blue.500"}
+          bgGradient={"linear(to-r, cyan.400, blue.500)"}
           bgClip={"text"}
-          color={"#000"}
         >
-          <Link to={"/"}>Product Store App</Link>
+          <Link to={"/"}>Product Store 🛒</Link>
         </Text>
         <HStack spacing={2} alignItems={"center"}>
           <Link to={"/products"}>
@@ -29,6 +38,9 @@ const Navbar = () => {
               <PlusSquareIcon w={6} h={6} />
             </Button>
           </Link>
+          <Button onClick={toggleColorMode}>
+            {colorMode === "light" ? <IoMoon /> : <LuSun size={20} />}
+          </Button>
         </HStack>
       </Flex>
     </Container>
--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -1,7 +1,86 @@
+import {
+  Box,
+  Button,
+  Container,
+  Heading,
+  useColorModeValue,
+  VStack,
+  Input,
+} from "@chakra-ui/react";
 import React from "react";
+import { useState } from "react";
 
 const CreatePage = () => {
-  return <div>CreatePage</div>;
+  const [newProduct, setNewProduct] = useState({
+    name: "",
+    image: "",
+    price: 0,
+  });
+
+  const handleAddProduct = async () => {
+    try {
+      const response = await fetch("http://localhost:5000/products", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newProduct),
+      });
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  return (
+    <Container maxW={"container.sm"}>
+      <VStack spacing={8}>
+        <Heading as="h1" fontSize={"2xl"} textAlign={"center"} mb={8} mt={20}>
+          Create a new product
+        </Heading>
+        <Box
+          w="full"
+          bg={useColorModeValue("white", "gray.800")}
+          p={6}
+          rounded={"lg"}
+          shadow={"md"}
+        >
+          <VStack spacing={5}>
+            <Input
+              type="text"
+              placeholder="Product name"
+              name="name"
+              value={newProduct.name}
+              onChange={(e) =>
+                setNewProduct({ ...newProduct, name: e.target.value })
+              }
+            />
+            <Input
+              type="text"
+              placeholder="Product image"
+              name="image"
+              value={newProduct.image}
+              onChange={(e) =>
+                setNewProduct({ ...newProduct, image: e.target.value })
+              }
+            />
+            <Input
+              type="number"
+              placeholder="Product price"
+              name="price"
+              value={newProduct.price}
+              onChange={(e) =>
+                setNewProduct({ ...newProduct, price: e.target.value })
+              }
+            />
+            <Button colorScheme="blue" onClick={handleAddProduct}>
+              Add product
+            </Button>
+          </VStack>
+        </Box>
+      </VStack>
+    </Container>
+  );
 };
 
 export default CreatePage;
